db712f98faceb0a8a73d962b7ca9c9fb72f0fabe
SindhuAgrahar2317 <168159597+SindhuAgrahar2317@users.noreply.github.com>
2025-07-31 10:24:27 +0530

    Create Super.java

---
+++ Super.java
@@ -0,0 +1,24 @@
+class Superdemo {
+    // Base class with a method to display a message
+    void display() {
+        System.out.println("This is the display method of the Super class.");
+    }
+}
+class Sub extends Superdemo {
+    // Overriding the display method
+    @Override
+    void display() {
+        // Calling the display method of the Super class using super    
+        super.display();
+        System.out.println("This is the display method of the Sub class.");
+    }
+}
+
+class Super extends Superdemo {
+    public static void main(String[] args) {
+        // Creating an instance of the Sub class
+        Sub sub = new Sub();
+        // Calling the display method, which will invoke the overridden method
+        sub.display();
+    }
+}
