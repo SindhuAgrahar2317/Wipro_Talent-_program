d24ad889206166bec7bd9787061403eabe82b43e
SindhuAgrahar2317 <168159597+SindhuAgrahar2317@users.noreply.github.com>
2025-07-31 10:16:57 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to find its factorial: ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+        sc.close();
+    }
+
+    public static int factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}   
